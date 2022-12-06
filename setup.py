7e5265ded7675519866d5dbb2e224a94efb02787
Jelmer van der Linde <jelmer@ikhoefgeen.nl>
2022-12-06 15:05:37 +0000

    Fix missing subpackages

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
 	name="tmxutil",
-	version="1.2",
+	version="1.3",
 	author="Jelmer van der Linde",
 	author_email="jelmer@ikhoefgeen.nl",
 	description="Tool to create, augment, filter and generally work with TMX files.",
@@ -25,6 +25,10 @@ setuptools.setup(
 			"tmxutil=tmxutil.cli:entrypoint",
 		],
 	},
-	packages=["tmxutil"],
+	packages=[
+		"tmxutil",
+		"tmxutil.filters",
+		"tmxutil.formats"
+	],
 	python_requires=">=3.6",
 )
